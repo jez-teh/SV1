291b47b4106df28d4fe7f99769b0e0e47e93e792
Asos75 <andraz.sosteric@gmail.com>
2026-05-04 12:22:58 +0200

    Added graphing function

--- code/util.py
+++ code/util.py
@@ -2,10 +2,14 @@ from datasets import Dataset
 import glob
 import os
 
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
 
 import torch
 import gc
+
+import numpy as np
+
 
 def load_datasets(train_path, val_path, test_path, label2id):
     train_dataset = Dataset.from_file(train_path)
@@ -53,27 +57,57 @@ def load_model_and_tokenizer(model_name: str, num_labels: int):
 
     return model, tokenizer
 
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    preds = np.argmax(logits, axis=-1)
+
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        labels, preds, average="binary"
+    )
+
+    acc = accuracy_score(labels, preds)
+
+    return {
+        "accuracy": acc,
+        "precision": precision,
+        "recall": recall,
+        "f1": f1
+    }
+
+
 def build_trainer(model, train_dataset, val_dataset,
-                  learning_rate=2e-5, num_epochs=3,
+                  learning_rate=2e-5,
+                  num_epochs=3,
                   per_device_train_batch_size=4,
                   per_device_eval_batch_size=4):
 
-    trainer = Trainer(
-        model=model,
-        args=TrainingArguments(
-            output_dir="./results",
-            eval_strategy="epoch",
-            learning_rate=learning_rate,
-            per_device_train_batch_size=per_device_train_batch_size,
-            per_device_eval_batch_size=per_device_eval_batch_size,
-            num_train_epochs=num_epochs,
-        ),
-        train_dataset=train_dataset,
-        eval_dataset=val_dataset,
+    args = TrainingArguments(
+        output_dir="./results",
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        logging_strategy="steps",
+        logging_steps=50,
+        learning_rate=learning_rate,
+        per_device_train_batch_size=per_device_train_batch_size,
+        per_device_eval_batch_size=per_device_eval_batch_size,
+        num_train_epochs=num_epochs,
+        load_best_model_at_end=True,
+        metric_for_best_model="f1",
+        report_to="none"  # keeps notebook clean
     )
 
-    return trainer
+    return Trainer(
+        model=model,
+        args=args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        compute_metrics=compute_metrics,
+    )
 
 def clear_cache(path):
     for filename in glob.glob(f"{path}/cache-*"):
+        os.remove(filename)
+
+def clear_folder(path):
+    for filename in glob.glob(f"{path}/*"):
         os.remove(filename)
